1eee6b78e4d8f734267f308b9ddf19ada1f75f3d
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2025-08-09 22:23:37 +0530

    multiple conditions

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import User from "./User";
 
 function App() {
 
-  const [display, setDisplay] = useState(true);
+  const [count, setCount] = useState(0);
 
   return (
     <div>
 
-      <h1>Toggle in React JS</h1>
-      <button onClick={() => setDisplay(!display)}>Toggle</button>
-      {/* {
-        display ? <h2>Tanveer</h2> : null
-      } */}
-
+      <h1>Multiple condition in React</h1>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Counter</button>
       {
-        display ? <User/> : null
+        count == 0 ? <h1>Condition 1</h1> 
+        : count == 1 ? <h1>Condition 2</h1> 
+        : count == 2 ? <h1>Condition 3</h1> 
+        : count == 3 ? <h1>Condition 4</h1> 
+        :null
       }
 
     </div>
