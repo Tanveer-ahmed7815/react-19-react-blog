b636a60eeb4b5f4cd02a82fe43c9bfb30c05f2ab
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2025-08-10 21:15:50 +0530

    controlled component

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,32 @@ import Wrapper from "./Wrapper";
 
 function App() {
 
-  const [val,setVal] = useState("Tanveer")
+  const [name,setName] = useState("")
+  const [password,setPassword] = useState("")
+  const [email,setEmail] = useState("")
 
   return (  
     <div>
 
-      <h1>Get input field value</h1>
-      <input type="text" value={val} onChange={(event)=>setVal(event.target.value)} placeholder="Enter username"/>
-      <h1>{val}</h1>
-      <button onClick={()=>setVal("")}>clear</button>
+      <h1>Controlled component</h1>
+      <form action="" method="get">
+
+        <input type="text" value={name} onChange={(event)=> setName(event.target.value)} placeholder="Enter name" />
+        <br /><br />
+        <input type="password" value={password} onChange={(event)=> setPassword(event.target.value)} placeholder="Enter password" />
+        <br /><br />
+        <input type="text" value={email} onChange={(event)=> setEmail(event.target.value)} placeholder="Enter email" />
+        <br /><br />
+        <button>Submit</button>
+
+        <button onClick={()=> {setName('');setPassword('');setEmail('')}}>Clear</button>
+
+        <h3>{name}</h3>
+        <h3>{password}</h3>
+        <h3>{email}</h3>
+
+      </form>
+   
     </div>
 
   )
