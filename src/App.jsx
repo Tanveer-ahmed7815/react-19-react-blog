73af5a6d8784612d3b5d863c9a7421735c7272fa
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2025-08-17 16:36:47 +0530

    External style css

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import User from "./User";
 import Clock from "./Clock";
 import College from "./College";
 import Counter from "./Counter";
+import './css/style.css'
 
 function App() {
 
@@ -11,16 +12,71 @@ function App() {
 
     <div>
 
-    <h1 style={{color:"red"}}>Inline style in React</h1>
-    <div>
-       <img style={{width:'200px'}} src="https://fastly.picsum.photos/id/145/536/354.jpg?hmac=Z4r_PULhGCx_ft_MM94T3oZEX_3u6Pvv9ip8ZoLveE8" alt="" />
-       <div>
-        <h4>Tanveer Ahmed</h4>
-        <p>Software developer</p>
-        
-       </div>
-    </div>
+      <h1 className="heading">External style in React</h1>
+      <div className="container">
+        <div className="user-card">
+          <div>
+            <img className="img-style" src="https://fastly.picsum.photos/id/145/536/354.jpg?hmac=Z4r_PULhGCx_ft_MM94T3oZEX_3u6Pvv9ip8ZoLveE8" alt="" />
+          </div>
+          <div className="text-wrap">
+            <h4>Piano</h4>
+          </div>
+        </div>
 
+        <div className="user-card">
+          <div>
+            <img className="img-style" src="https://fastly.picsum.photos/id/145/536/354.jpg?hmac=Z4r_PULhGCx_ft_MM94T3oZEX_3u6Pvv9ip8ZoLveE8" alt="" />
+          </div>
+          <div className="text-wrap">
+            <h4>Piano</h4>
+          </div>
+        </div>
+
+        <div className="user-card">
+          <div>
+            <img className="img-style" src="https://fastly.picsum.photos/id/145/536/354.jpg?hmac=Z4r_PULhGCx_ft_MM94T3oZEX_3u6Pvv9ip8ZoLveE8" alt="" />
+          </div>
+          <div className="text-wrap">
+            <h4>Piano</h4>
+          </div>
+        </div>
+
+        <div className="user-card">
+          <div>
+            <img className="img-style" src="https://fastly.picsum.photos/id/145/536/354.jpg?hmac=Z4r_PULhGCx_ft_MM94T3oZEX_3u6Pvv9ip8ZoLveE8" alt="" />
+          </div>
+          <div className="text-wrap">
+            <h4>Piano</h4>
+          </div>
+        </div>
+
+        <div className="user-card">
+          <div>
+            <img className="img-style" src="https://fastly.picsum.photos/id/145/536/354.jpg?hmac=Z4r_PULhGCx_ft_MM94T3oZEX_3u6Pvv9ip8ZoLveE8" alt="" />
+          </div>
+          <div className="text-wrap">
+            <h4>Piano</h4>
+          </div>
+        </div>
+
+        <div className="user-card">
+          <div>
+            <img className="img-style" src="https://fastly.picsum.photos/id/145/536/354.jpg?hmac=Z4r_PULhGCx_ft_MM94T3oZEX_3u6Pvv9ip8ZoLveE8" alt="" />
+          </div>
+          <div className="text-wrap">
+            <h4>Piano</h4>
+          </div>
+        </div>
+
+        <div className="user-card">
+          <div>
+            <img className="img-style" src="https://fastly.picsum.photos/id/145/536/354.jpg?hmac=Z4r_PULhGCx_ft_MM94T3oZEX_3u6Pvv9ip8ZoLveE8" alt="" />
+          </div>
+          <div className="text-wrap">
+            <h4>Piano</h4>
+          </div>
+        </div>
+      </div>
     </div>
 
   )
