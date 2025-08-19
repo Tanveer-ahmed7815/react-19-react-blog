ee414e8a3cb16ea4292739d064b54c31e0537e52
Tanveer-ahmed7815 <tanveer.ahmed7815@gmail.com>
2025-08-19 10:38:18 +0530

    useref with uncontrolled components

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,50 @@ import { useRef } from "react"
 
 function App() {
 
-  const inputRef = useRef(null);
-  const inputHandler = () => {
-    console.log(inputRef);
-    inputRef.current.focus();
-    inputRef.current.style.color = 'red'
-  }
+  const userRef = useRef();
+  const passwordRef = useRef();
 
+  const handleForm = (event)=>{
+    event.preventDefault();
+    const user = document.querySelector("#user").value;
+    const password = document.querySelector("#password").value;
+    console.log(user,password);
+  }
+  
+  const handleFormRef = (event)=>{
+    event.preventDefault();
+    const user = userRef.current.value;
+    const password = passwordRef.current.value;
+    console.log(user,password);
+  }
 
   return (
 
     <div>
 
-      <h1 >useRef in React JS</h1>
-      <input ref={inputRef} type="text" placeholder="Enter username" />
-      <button onClick={inputHandler}>Focus on Input field</button>
+      <h1 >Uncontrolled component in React JS</h1>
+      <form action="" method="get" onSubmit={handleForm}>
+        <input type="text" id="user" placeholder="Enter user name" />
+        <br /><br />
+        <input type="password" id="password" placeholder="Enter user password" />
+        <br /><br />
+        <button>
+          Submit
+        </button>
+      </form>
+
+      <hr />
+  
+      <h1 >Uncontrolled component in React JS with useRef</h1>
+      <form action="" method="get" onSubmit={handleFormRef}>
+        <input type="text" ref={userRef} id="userRef" placeholder="Enter user name" />
+        <br /><br />
+        <input type="password" ref={passwordRef} id="passwordRef" placeholder="Enter user password" />
+        <br /><br />
+        <button>
+          Submit with Ref
+        </button>
+      </form>
 
     </div>
 
